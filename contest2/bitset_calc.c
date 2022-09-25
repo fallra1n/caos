22161bfcd316cda033cc05d072a1ec896e6bb0f3
asadgod <tozhimukhammedov.aa@yandex.ru>
2022-09-26 02:49:23 +0300

    a

--- contest2/bitset_calc.c
+++ contest2/bitset_calc.c
@@ -1,38 +1,48 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main() {
+int main()
+{
     int64_t base = 0;
     int64_t cur = 0;
 
     char temp_char;
     int a = scanf("%c", &temp_char);
-    while (a > 0 && (temp_char != '\0' && temp_char != '\n')) {
-        if (temp_char == '&') {
+    while (a > 0 && (temp_char != '\0' && temp_char != '\n'))
+    {
+        if (temp_char == '&')
+        {
             base &= cur;
             cur = 0;
         }
-        else if (temp_char == '|') {
+        else if (temp_char == '|')
+        {
             base |= cur;
             cur = 0;
         }
-        else if (temp_char == '^') {
+        else if (temp_char == '^')
+        {
             base ^= cur;
             cur = 0;
         }
-        else if (temp_char == '~') {
+        else if (temp_char == '~')
+        {
             base ^= 4611686018427387903;
             cur = 0;
         }
-        else {
+        else
+        {
             int64_t a = 1;
-            if (temp_char >= '0' && temp_char <= '9') {
+            if (temp_char >= '0' && temp_char <= '9')
+            {
                 a <<= (int)temp_char - '0';
             }
-            if (temp_char >= 'A' && temp_char <= 'Z') {
+            if (temp_char >= 'A' && temp_char <= 'Z')
+            {
                 a <<= (int)temp_char - 'A' + 10;
             }
-            if (temp_char >= 'a' && temp_char <= 'z') {
+            if (temp_char >= 'a' && temp_char <= 'z')
+            {
                 a <<= (int)temp_char - 'a' + 36;
             }
 
@@ -43,15 +53,20 @@ int main() {
     }
 
     int i = 1;
-    while (base != 0 && i <= 62) {
-        if (base % 2 == 1) { 
-            if (i >= 1 && i <= 10) {
+    while (base != 0 && i <= 62)
+    {
+        if (base % 2 == 1)
+        {
+            if (i >= 1 && i <= 10)
+            {
                 printf("%c", (char)(i - 1 + '0'));
             }
-            else if (i >= 11 && i <= 36){
+            else if (i >= 11 && i <= 36)
+            {
                 printf("%c", (char)(i - 1 - 10 + 'A'));
             }
-            else{
+            else
+            {
                 printf("%c", (char)(i - 1 - 36 + 'a'));
             }
         }
