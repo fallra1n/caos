7ce80bf65d9cfd3c1db1e5c32aa49b57e1a89459
asadgod <tozhimukhammedov.aa@yandex.ru>
2022-09-28 00:46:30 +0300

    aaa

--- contest2/bitset_calc.c
+++ contest2/bitset_calc.c
@@ -1,76 +1,73 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main()
-{
-    int64_t base = 0;
-    int64_t cur = 0;
-
+void ExpressionProcessing(int64_t *base, int64_t *cur, int8_t end_of_digits, int8_t end_of_uppercase) {
     char temp_char;
     int a = scanf("%c", &temp_char);
-    while (a > 0 && (temp_char != '\0' && temp_char != '\n'))
-    {
-        if (temp_char == '&')
-        {
-            base &= cur;
-            cur = 0;
-        }
-        else if (temp_char == '|')
-        {
-            base |= cur;
-            cur = 0;
-        }
-        else if (temp_char == '^')
-        {
-            base ^= cur;
-            cur = 0;
-        }
-        else if (temp_char == '~')
-        {
-            base ^= 4611686018427387903;
-            cur = 0;
-        }
-        else
-        {
-            int64_t a = 1;
-            if (temp_char >= '0' && temp_char <= '9')
-            {
-                a <<= (int)temp_char - '0';
-            }
-            if (temp_char >= 'A' && temp_char <= 'Z')
-            {
-                a <<= (int)temp_char - 'A' + 10;
-            }
-            if (temp_char >= 'a' && temp_char <= 'z')
-            {
-                a <<= (int)temp_char - 'a' + 36;
-            }
+    int64_t serial_number = 1;
 
-            cur |= a;
-        }
+    while (a > 0 && (temp_char != '\0' && temp_char != '\n')) {
+        switch (temp_char) {
+            case '&':
+                *base &= *cur;
+                *cur = 0;
+                break;
+            case '|':
+                *base |= *cur;
+                *cur = 0;
+                break;
+            case '^':
+                *base ^= *cur;
+                *cur = 0;
+                break;
+            case '~':
+                *base ^= 4611686018427387903; // xor with 2^62 - 1
+                *cur = 0;
+                break;
+            default:
+                serial_number = 1;
+                if (temp_char >= '0' && temp_char <= '9') {
+                    serial_number <<= (int)temp_char - '0';
+                }
+                if (temp_char >= 'A' && temp_char <= 'Z') {
+                    serial_number <<= (int)temp_char - 'A' + end_of_digits;
+                }
+                if (temp_char >= 'a' && temp_char <= 'z') {
+                    serial_number <<= (int)temp_char - 'a' + end_of_uppercase;
+                }
 
+                *cur |= serial_number;
+        }
         a = scanf("%c", &temp_char);
     }
+}
 
-    int i = 1;
-    while (base != 0 && i <= 62)
-    {
-        if (base % 2 == 1)
-        {
-            if (i >= 1 && i <= 10)
-            {
+void PrintTheFinalSet(int64_t *base, int8_t end_of_digits, int8_t end_of_uppercase, int8_t cardinality_of_the_set) {
+    int8_t i = 1;
+    while (*base != 0 && i <= cardinality_of_the_set) {
+        if (*base % 2 == 1) {
+            if (i >= 1 && i <= end_of_digits) {
                 printf("%c", (char)(i - 1 + '0'));
             }
-            else if (i >= 11 && i <= 36)
-            {
-                printf("%c", (char)(i - 1 - 10 + 'A'));
+            else if (i >= end_of_digits + 1 && i <= end_of_uppercase) {
+                printf("%c", (char)(i - 1 - end_of_digits + 'A'));
             }
-            else
-            {
-                printf("%c", (char)(i - 1 - 36 + 'a'));
+            else {
+                printf("%c", (char)(i - 1 - end_of_uppercase + 'a'));
             }
         }
-        base >>= 1;
+        *base >>= 1;
         ++i;
     }
+}
+
+int main() {
+    int64_t base = 0;
+    int64_t cur = 0;
+    int8_t cardinality_of_the_set = 62;
+    int8_t end_of_digits = 10;
+    int8_t end_of_uppercase = 36;
+    ExpressionProcessing(&base, &cur, end_of_digits, end_of_uppercase);
+    PrintTheFinalSet(&base, end_of_digits, end_of_uppercase, cardinality_of_the_set);
+    return 0;
 }
--- contest2/classify.c
+++ contest2/classify.c
@@ -1,93 +1,49 @@
 #include <stdint.h>
-#include <stdio.h>
 
 typedef enum {
-    PlusZero      = 0x00,
-    MinusZero     = 0x01,
-    PlusInf       = 0xF0,
-    MinusInf      = 0xF1,
-    PlusRegular   = 0x10,
-    MinusRegular  = 0x11,
-    PlusDenormal  = 0x20,
+    PlusZero = 0x00,
+    MinusZero = 0x01,
+    PlusInf = 0xF0,
+    MinusInf = 0xF1,
+    PlusRegular = 0x10,
+    MinusRegular = 0x11,
+    PlusDenormal = 0x20,
     MinusDenormal = 0x21,
-    SignalingNaN  = 0x30,
-    QuietNaN      = 0x31
+    SignalingNaN = 0x30,
+    QuietNaN = 0x31
 } float_class_t;
 
 extern float_class_t classify(double *value_ptr) {
     double *value_ptr_as_double = value_ptr;
     void *value_ptr_as_void = value_ptr_as_double;
-    int64_t *value_ptr_as_int = value_ptr_as_void;
+    uint64_t *value_ptr_as_int = value_ptr_as_void;
 
-    int64_t sign = *value_ptr_as_int | 4607182418800017408; // bit or с 1.0 тем самым вытаскиваем знак
+    uint64_t sign = *value_ptr_as_int;
+    sign >>= 63;
 
-    // search next 11 bit
-    int64_t temp = *value_ptr_as_int;
+    uint64_t exp = *value_ptr_as_int;
+    exp <<= 1;
+    exp >>= 53;
 
-    int64_t exp = 0;
-    int64_t mn = 10;
-
-    for (int64_t i = 0; i < 11; ++i) {
-        temp <<= 1;
-        int64_t first_bit = temp | 4607182418800017408; // 1
-        if (first_bit > 0) {
-            exp *= mn;
-        } else {
-            exp *= mn;
-            exp += 1;
-        }
-    }
-
-    int man_is_zero = 1;
-
-    for (int64_t i = 0; i < 52; ++i) {
-        temp <<= 1;
-        int64_t first_bit = temp | 4607182418800017408; // 1
-        if (first_bit < 0) {
-            man_is_zero = 0;
-        }
-    }
+    uint64_t man = *value_ptr_as_int;
+    man <<= 12;
+    man >>= 12;
 
     if (exp == 0) {
-        if (man_is_zero) {
-            if (sign > 0) {
-                return PlusZero;
-            }
-            
-            return MinusZero;
+        if (man == 0) {
+            return sign == 0 ? PlusZero : MinusZero;
         }
 
-        if (sign > 0) {
-            return PlusDenormal;
-        }
-
-        return MinusDenormal;
+        return sign == 0 ? PlusDenormal : MinusDenormal;
     }
 
-    if (exp == 11111111111) {
-        if (man_is_zero) {
-            if (sign > 0) {
-                return PlusInf;
-            }
-            
-            return MinusInf;
+    if (exp == 2047) {
+        if (man == 0) {
+            return sign == 0 ? PlusInf : MinusInf;
         }
 
-        if (sign > 0) {
-            return SignalingNaN;
-        }
-
-        return QuietNaN;
+        return sign == 0 ? SignalingNaN : QuietNaN;
     }
 
-    if (sign > 0) {
-        return PlusRegular;
-    }
-
-    return MinusRegular;
-}
-
-int main() {
-    double value = 0.155625;
-    printf("%ld", classify(&value));
+    return sign == 0 ? PlusRegular : MinusRegular;
 }
