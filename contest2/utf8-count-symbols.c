984a24c58c6a95107bf3b6b476eaf2c559c7e07d
asadgod <tozhimukhammedov.aa@yandex.ru>
2022-10-03 02:17:39 +0300

    aa

--- contest2/utf8-count-symbols.c
+++ contest2/utf8-count-symbols.c
@@ -1,65 +1,58 @@
 #include <stdio.h>
 #include <stdint.h>
 
-int main() {
+const uint8_t END_OF_ASCII = 127;
+const uint8_t ASCII_FIRST_BIT = 0;
+const uint8_t GET_FIRST_BIT = 7;
+const uint8_t GET_FIRST_TWO_BITS = 6;
+const uint8_t MINOR_BITS_HEADER = 2;
+const uint8_t FALSE = 0;
+const uint8_t TRUE = 1;
+
+struct AmountOfSymbols {
+  int32_t fits_under_utf8;
+  int32_t unsuitable_under_utf8;
+};
+
+int SymbolHandler(uint8_t symbol_as_int) {
+  uint8_t is_crush = FALSE;
+  uint8_t amount_of_bytes = 0;
   
-  int fit = 0;
-  int unsuitable = 0;
-
-  char symbol;
-  int cnt = scanf("%c", &symbol);
-
-  while (symbol != EOF && cnt > 0) {
-    uint32_t symbol_as_int = (int)symbol;
-    uint32_t first = symbol_as_int >> 24;
-    uint32_t second = symbol_as_int << 8 >> 24;
-    uint32_t third = symbol_as_int << 16 >> 24;
-    uint32_t fourth = symbol_as_int << 24 >> 24;
-
-    if (first >> 3 == 30) { // == 11110
-      if (second >> 6 != 2 || third >> 6 != 2 || fourth >> 6 != 2) {
-        printf("%d %d", fit, unsuitable);
-        return 1;
-      }
-      ++unsuitable;
-      cnt = scanf("%c", &symbol);
-      continue;
-    }
-
-    if (second >> 4 == 14) { // == 1110
-      if (first != 0 || third >> 6 != 2 || fourth >> 6 != 2) {
-        printf("%d %d", fit, unsuitable);
-        return 1;
-      }
-      ++unsuitable;
-      cnt = scanf("%c", &symbol);
-      continue;
-    }
-
-    if (third >> 5 == 6) { // == 110
-      if (first != 0 || second != 0 || fourth >> 6 != 2) {
-        printf("%d %d", fit, unsuitable);
-        return 1;
-      }
-      ++unsuitable;
-      cnt = scanf("%c", &symbol);
-      continue;
-    }
-
-    if (fourth >> 7 != 0) {
-      printf("%d %d", fit, unsuitable);
-      return 1;
-    }
-
-    if (first != 0 || second != 0 || third != 0) {
-      printf("%d %d", fit, unsuitable);
-      return 1;
-    }
-
-    ++fit;
-    cnt = scanf("%c", &symbol);
+  int32_t i = 0;
+  while (symbol_as_int >> (GET_FIRST_BIT - i) == 1) {
+    ++amount_of_bytes;
+    ++i;
   }
-  
-  printf("%d %d", fit, unsuitable);
+
+  char temp;
+  for (i = 0; i < amount_of_bytes; ++i) {
+    if ((temp = getchar()) == EOF || (uint8_t)temp >> GET_FIRST_TWO_BITS != MINOR_BITS_HEADER) {
+      is_crush = TRUE;
+      break;
+    }
+  }
+
+  return is_crush;
+}
+
+void SequenceHandler(struct AmountOfSymbols *result) {
+  char symbol;
+
+  while ((symbol = getchar()) != EOF) {    
+    if ((uint8_t)symbol >> GET_FIRST_BIT == ASCII_FIRST_BIT) {
+      ++(result->fits_under_utf8);
+    } else {
+      if (SymbolHandler((uint8_t)symbol)) {
+        return;
+      }
+      ++(result->unsuitable_under_utf8);
+    }
+  }
+}
+
+int main() {
+  struct AmountOfSymbols result = {0, 0};
+  SequenceHandler(&result);
+  printf("\n%d %d\n", result.fits_under_utf8, result.unsuitable_under_utf8);
   return 0;
 }
