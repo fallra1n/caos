8753967b088aa1a1ccd9cb5ecb68b0e4eae3eab6
asadgod <tozhimukhammedov.aa@yandex.ru>
2022-11-14 16:02:16 +0300

    fix copy-stream and implemented copy_stream in asm

--- contest7/copy-stream/copy_stream.c
+++ contest7/copy-stream/copy_stream.c
@@ -2,16 +2,19 @@
 
 extern long syscall(long number, ...);
 
-const long COUNT = 512 * 1024; // 0,5 Mb
+const long COUNT = 64 * 1024; // 64 Kb
 
 void _start() {
   char text[COUNT];
-  syscall(SYS_brk, text + COUNT);
-  long success_bytes = 0; 
+  long success_bytes_r = 0;
+  long success_bytes_w = 0;
   do {
-    success_bytes = syscall(SYS_read, 0, text, COUNT);
-    syscall(SYS_write, 1, text, success_bytes);
-  } while (success_bytes != 0);
+    success_bytes_r = syscall(SYS_read, 0, text, COUNT);
+
+    if (success_bytes_r != -1) {
+      success_bytes_w = syscall(SYS_write, 1, text, success_bytes_r);
+    }
+  } while (success_bytes_r > 0 && success_bytes_w > 0);
   
   syscall(SYS_exit, 0);
 }
---
+++ contest7/copy-stream_asm/run.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+make
+
+echo
+echo ===================================================
+echo
+
+./result.o
+rm -rf copy_stream.o
+rm -rf syscall.o
+rm -rf result.o
